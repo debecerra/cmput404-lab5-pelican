09d76c0af10769515c39ecc1fde167cd72fa3f5a
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-10-06 18:22:01 -0600

    minor

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://github.com/debecerra/cmput404-lab5-pelican'
+SITEURL = 'https://debecerra.github.io/cmput404-lab5-pelican/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
